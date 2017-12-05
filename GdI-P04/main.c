83e1ceb1b86102a8c7fbb4ee057af66f94cb09b3
larisf <lars.isf@gmx.de>
2017-12-05 15:00:42 +0100

    Aufgabe V4.2 - Evtl. Zusataufgaben hinzugefuegt

--- GdI-P04/main.c
+++ GdI-P04/main.c
@@ -8,8 +8,8 @@ struct Knoten //Struktur für die Liste
 	struct Knoten* next; //zeiger auf die nächste Struktur vom selben Typen und den selben Mitgliedern/Variablen
 }*anfang, *liste; //Strukturen namen
 /*Strukturen anlegen*/
-	struct Knoten *anfang; //Struktur-Name *anfang
-	struct Knoten *liste; //Struktur-Name *liste
+//	struct Knoten *anfang; //Struktur-Name *anfang
+//	struct Knoten *liste; //Struktur-Name *liste
 /*--------------*/
 /*Bekanntmachen der Funktionen*/
 void qinit();
@@ -17,6 +17,7 @@ void enqeue(int element);
 int deqeue(void);
 int isempty(void);
 void printQeue(void);
+int anzElemente;
 /*----------------------------*/
 int main() //Menue und Switch-Case
 {
@@ -29,7 +30,10 @@ int main() //Menue und Switch-Case
 				"1. Anfuegen eines Elementes\n"
 				"2. Loeschen und Ausgeben des ersten Elementes\n"
 				"3. Ausgeben der Warteschlange\n"
-				"4. Beenden des Programms\n\n"
+				"4. Beenden des Programms\n"
+				/*"5. Sortieren -> Absteigend\n"
+				"6. Sortieren -> Aufsteigend\n"
+				"7. Beliebige Zahl loeschen\n\n"*/
 				"Bitte geben Sie die Ziffer der gewuenschten Funktion ein:");
 		scanf("%d",&auswahl); //einlesen des Switch-Case Parameters
 		switch(auswahl) //Switch-Case
@@ -56,6 +60,23 @@ int main() //Menue und Switch-Case
 				printf("Ende"); //Ausgabe bei beenden des Programmes
 				return (EXIT_SUCCESS); //Abbruch Bedingung
 			}
+			case 5:
+			{
+				sortQeueAbsteigend();
+				printQeue();
+				break;
+			}
+			case 6:
+			{
+				sortQeueAufsteigend();
+				printQeue();
+				break;
+			}
+			case 7:
+			{
+				chosenDeqeue();
+				break;
+			}
 			default:
 				printf("Ungueltige Eingabe!\n"); //Ausgabe bei falscher Eingabe bzw. eines Wertes >= 4
 		}
@@ -75,6 +96,7 @@ void enqeue(int element) //element anhaengen
 		anfang->key = element; //einzufügendes Element setzen
 		anfang->next = NULL; // next pointer auf NULL setzen
 		liste = anfang; //liste auf anfang setzen
+		anzElemente++;
 	}
 	else //Element anfügen -> Warteschlange nicht leer
 	{
@@ -84,6 +106,7 @@ void enqeue(int element) //element anhaengen
 		liste = liste->next; //liste auf freie Stelle setzen
 		liste->key = element; //element einfügen
 		liste->next = NULL; //nächstes Element auf NULL setzen
+		anzElemente++;
 	}
 }
 int deqeue() //element loeschen
@@ -119,4 +142,81 @@ void printQeue() //Ausgabe der Liste
 	}
 	else
 		return printf("Leer: %d\n",isempty()); //ausgabe, wenn die liste leer ist
+}
+void sortQeueAbsteigend()
+{
+	int vorgaenger=0;
+	liste = anfang;
+	if(liste != NULL)
+	{
+		for(int i= 0;i <= anzElemente;i++)
+		{	
+			liste = anfang;
+			while(liste->next != NULL)
+			{
+				if(liste->key < liste->next->key)
+				{
+					vorgaenger = liste->key;
+					liste->key = liste->next->key;
+					liste->next->key = vorgaenger;				
+				}
+				liste = liste->next;
+			}
+		}
+	}
+}
+void sortQeueAufsteigend()
+{
+	int vorgaenger=0;
+	liste = anfang;
+	if(liste != NULL)
+	{
+		for(int i= 0;i <= anzElemente;i++)
+		{	
+			liste = anfang;
+			while(liste->next != NULL)
+			{
+				if(liste->key > liste->next->key)
+				{
+					vorgaenger = liste->key;
+					liste->key = liste->next->key;
+					liste->next->key = vorgaenger;				
+				}
+				liste = liste->next;
+			}
+		}
+	}
+}
+
+int chosenDeqeue()
+{
+	struct Knoten* temp;
+	int eingabe=0;
+	liste = anfang; //liste auf anfang setzen
+	if(liste != NULL) //erste Element aus der Warteschlange löschen
+	{
+		printf("zu loeschenden Wert eingeben: \n");
+		scanf("%d",&eingabe);
+		if(anfang->key == eingabe)
+			{
+				liste = liste->next;
+				free(anfang);
+				anfang = liste;
+			}
+		else
+		{
+			liste = anfang;
+			while(liste->next != NULL)
+			{
+				temp = liste->next;
+				if(liste->next->key == eingabe)
+				{
+					liste->next = temp->next;
+					free(temp);
+					break;
+				}
+				liste = temp;
+			}	
+		}
+	}
 }
