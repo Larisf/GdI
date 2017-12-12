f799e4863086b3bd459c04e9a2adaf7bc2355635
larisf <lars.isf@gmx.de>
2017-12-12 22:21:18 +0100

    Aufgabe 5 V1.5 - Too Easy

--- GdI-P05/main.c
+++ GdI-P05/main.c
@@ -14,40 +14,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 const int MAX=32767; //Maximale länge des strings
-void tabulator(int argc); //bekanntmachen der tabulator funktion
+void expand(FILE *datei);
 int main(int argc, char** argv) 
 {
 	setbuf(stdout, NULL); //Output_Buffer auf NULL setzen
-	tabulator(argc); //Funktion für den Algorithmus
+	FILE *datei; //File typ mit einem Zeiger für die Datei
+	if(argc < 2 || (datei = fopen(argv[1],"r")) == NULL) //wenn input der Argumente beim programmstart < 2 dann:
+	{
+		printf("Keine Datei gefunden! Bitte eingabe taetigen:\n"); //Ausgabe, wenn keine Datei angegeben oder gefunden wurde
+		expand(stdin); //rufe expand auf
+	}
+	else if((datei = fopen(argv[1],"r")) != NULL) //argc > 2 dann öffne die datei mit dem Namen "NAME" zum lesen, wenn vorhanden
+		expand(datei); //rufe expand auf
 	return (EXIT_SUCCESS);
 }
-void tabulator(int argc) //Funktion für den Algorithmus
+void expand(FILE *datei) //expand funktion convertiere \t zu leerzeichen
 {
-	char satz[MAX], satzteil[MAX]; //Arrays für die Eingabe/Inhalte der Datei
-	FILE *datei; //File typ mit einem Zeiger für die Datei
-	if(argc < 2) //wenn input der Argumente beim programmstart < 2 dann:
+	int c=0; 
+	while((c=fgetc(datei)) != EOF) //solange bis das datei ende erreicht wurde
 	{
-		printf("Keine Datei gefunden, bitte taetigen Sie eine Eingabe:\n"); //ausgabe, wenn argumente <2
-		fgets(satz,MAX,stdin); //pointer auf die stelle wo es hin kopiert wird mit einer maximalen laenger MAX aus der Standarteingabe
+		if(c=='\\') //ist c = '\'
+			if((c=fgetc(datei))=='t') //ist der darauf folgende character = t
+				for(int i=0; i<7;i++) //solange ausführen bis i = 6 (7x dank der 0)
+				{
+					c = ' '; //c = leerzeichen setzen
+					putchar(c); //leezeichen in console schreiben
+				}
+		putchar(c); //character der datei in console schreiben
 	}
-	else if((datei = fopen("test.txt","r")) != NULL) //argc > 2 dann öffne die datei mit dem Namen "NAME" zum lesen, wenn vorhanden
-	{
-		fgets(satz,MAX,datei); //pointer auf die stelle wo es hin kopiert wird mit einer maximalen laenger MAX aus der Datei
-		fclose(datei); //datei schließen
-	}
-	for(int i=0,j = 0; i <MAX;i++,j++) //algorithmus um \t durch Leerzeichen zu ersetzen
-	{
-		satzteil[i] = satz[j]; //eingelesenen string in zweiten stringkopieren
-		if(satz[j] == '\\' && satz[j+1] == 't') //ist ein \t erreicht dann:
-		{	
-			j+=2; //eingelesenen string um 2stellen nach rechts rücken um das \t loszuwerden
-			for(int z= 0; z<8;z++) //8 mal durchlaufen
-			{
-				satzteil[i] = ' '; //die i-te stelle mit einem leerzeichen belegen
-				i++; //i inkrementieren
-			}
-		}
-		satzteil[i] = satz[j]; //eingelesenen string an der stelle nach dem \t an zweiten string anfügen
-	}
-	printf("%s",satzteil); //ausgabe
 }
