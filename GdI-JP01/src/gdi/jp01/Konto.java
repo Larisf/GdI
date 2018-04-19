1b7dccb2001065aebfd3e1048a6ea96c2c4f4e02
larisf <lars.isf@gmx.de>
2018-04-19 09:15:23 +0200

    Full-1

--- GdI-JP01/src/gdi/jp01/Konto.java
+++ GdI-JP01/src/gdi/jp01/Konto.java
@@ -1,27 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gdi.jp01;
 
 /**
- * Write a description of class Konto here.
+ * Klasse zum erstellen eines Kontos.
  *
  * @author Lars Isferding
  * @version 13.4.2018
  */
-public class Konto 
-{
-    /**
-     * @param args the command line arguments
-     */
+package gdi.jp01;
+public class Konto {
     private int kontostand;
     private int kontoNr;
     private Inhaber inhaber;
+    private int dispo;
+    
     /**
      * Konstruktor zum eröffnen eines neuen Kontos
-     * @param int kontoNr String vorname String nachname String adresse
+     * @param kontoNr Kontonummer angeben 
+     * @param vorname Vornamen angeben 
+     * @param nachname Nachnamen angeben 
+     * @param adresse Adresse angeben
      */
     Konto(int kontoNr, String vorname, String nachname, String adresse) //Konto eröffnen
     {
@@ -29,9 +25,14 @@ public class Konto
         inhaber = new Inhaber(vorname,nachname,adresse);
     }
         
+    
     /**
-     * Konstruktor zum eröffnen eines Kontos und direkter Einzahlung eines Betrages
-     * @param int KontoNr String vorname String nachname String adresse int einzahlung
+     * Konstruktor zum eröffnen eines neuen Kontos
+     * @param kontoNr Kontonummer angeben 
+     * @param vorname Vornamen angeben 
+     * @param nachname Nachnamen angeben 
+     * @param adresse Adresse angeben
+     * @param einzahlung betrag zum einzahlen angeben
      */
     Konto(int kontoNr, String vorname, String nachname, String adresse, int einzahlung) //Konto eröffnen mit Einzahlung
     {
@@ -39,9 +40,15 @@ public class Konto
         this.kontostand = einzahlung;
         inhaber = new Inhaber(vorname,nachname,adresse);
     }
+    
+    
     /**
-     * Konstruktor zum eröffnen eines Kontos und Freundschaftspauschale für ein anderes Konto
-     * @param int kontoNr String vorname String nachname String adresse Konto empfaenger
+     * Konstruktor zum eröffnen eines neuen Kontos
+     * @param kontoNr Kontonummer angeben 
+     * @param vorname Vornamen angeben 
+     * @param nachname Nachnamen angeben 
+     * @param adresse Adresse angeben
+     * @param empfaenger Empfaenger Konto angeben
      */
     Konto(int kontoNr, String vorname, String nachname, String adresse, Konto empfaenger) //Konto eroeffnen mit Freundschaftsbonus von 60€ fuer ein anderes Konto
     {
@@ -49,61 +56,96 @@ public class Konto
         empfaenger.einzahlen(60);
         inhaber = new Inhaber(vorname,nachname,adresse);
     }
-    public static void main(String[] args) {}
+
+    public static void main(String[] args) 
+    {
+    }
+
     /**
-     * @return    Gibt den Kontostand aus
+     * Funktion zum ausgeben des Kontostands
+     * @return int kontostand Gibt den Kontostand aus
      */
     public int getKontostand() //Kontostand rausgeben
     {
         return kontostand;
     }
+
     /**
      * Funktion zum einzahlen eines Betrages.
-     * @param int kontostand
+     * @param int kontostand Kontostand um gewissen Betrag erhöhen
      */
     public void einzahlen(int kontostand) //Geld einzahlen
     {
         this.kontostand += kontostand;
     }
+
     /**
-     * Funktion zum abheben eines Betrages.
-     * @param int kontostand
+     * Funktion zum abheben eines Betrages unter Berücksichtigung des Dispos.
+     * 
+     * @param int abheben Betrag, welcher abgehoben werden soll.
      */
-    public void abheben(int kontostand) //Geld abheben
+    public void abheben(int abheben) //Geld abheben
     {
-        this.kontostand -= kontostand;
+        if((kontostand+dispo) >= (abheben))
+            this.kontostand -= abheben;
+        else
+            System.out.println("Dispo nicht ausreichend");
     }
+    
     /**
      * Funktion zum überweisen von A nach B
-     * @param int betrag, Konto empfaenger
+     * @param betrag Betrag welcher überwiesen werden soll
+     * @param empfaenger Empfaenger Konto auswählen
      */
     public void ueberweisen(int betrag, Konto empfaenger) //Geld von A nach B ueberweisen
     {
         abheben(betrag);
         empfaenger.einzahlen(betrag);
     }
+    
     /**
-     * @return Gibt die Kontonummer aus
+     * Funktion zum ausgeben der Kontonummer
+     * @return int kontoNr Gibt die Kontonummer aus
      */
     public int getKontoNr() //KontoNr ausgeben
     {
         return kontoNr;
     }
+
     /**
      * Funktion zum setzen einer Kontonummer.
-     * @param int kontoNr
+     * @param int kontoNr setzen der Kontonummer
      */
     public void setKontoNr(int kontoNr) //KontoNr Setzen
     {
         this.kontoNr = kontoNr;
     }
+
     /**
      * Funktion zum ausgeben des Inhabers.
-     * @return inhaber
+     * @return inhaber Inhaber ausgeben
      */
     public Inhaber getInhaber() //Inhaber ausgeben (Konsole)
     {
         //System.out.println(inhaber.getVorname()+"\n"+inhaber.getNachname()+"\n"+inhaber.getAdresse());
         return inhaber;
     }
+    
+    /**
+     *  Funktion zum Inanspruch nehmen eines Dispos
+     *  @param int dispo setzen einer Dispogrenze
+     */
+    public void setDispo(int dispo)
+    {
+        this.dispo = dispo;
+    }
+   
+    /**
+     * Funktion zum ausgeben des Dispos
+     * @return int dispo ausgeben der Dispogrenze
+     */
+   public int getDispo()
+   {
+        return dispo;    
+   }
 }
